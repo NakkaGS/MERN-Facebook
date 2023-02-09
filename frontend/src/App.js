ea61deff8f565484f0d92d4430098d08801465e9
NakkaGS <gtnakata@gmail.com>
2023-02-09 21:35:47 +0100

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { LiveVideo } from "./svg"
+function App() {
+  return (
+    <div>
+      Welcome to frontend
+      <LiveVideo color='red' />
+    </div>
+  );
+}
+
+export default App;
