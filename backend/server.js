18b2c59d9f1617080e916286d34f4672ba9ce172
NakkaGS <gtnakata@gmail.com>
2023-02-09 21:50:46 +0100

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.get('/', (req,res) => {
     res.send('Welcome from Home')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import { LiveVideo } from "./svg"
 function App() {
+  const get = async () => {
+    const res = await fetch('http://localhost:8000/books')
+    console.log(res)
+  }
+  get()
   return (
     <div>
       Welcome to frontend
