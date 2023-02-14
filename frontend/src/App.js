5dd6cc024cb25c2fa20e104d0da3667cba99e26b
NakkaGS <gtnakata@gmail.com>
2023-02-14 21:33:50 +0100

    Frontend: Router added and some pages to route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import { LiveVideo } from "./svg"
+import { Routes, Route } from 'react-router-dom'
+
+//Pages
+import Login from './pages/login'
+import Profile from './pages/profile'
+import Home from './pages/home'
+
 function App() {
-  const get = async () => {
-    const res = await fetch('http://localhost:8000/books')
-    console.log(res)
-  }
-  get()
-  return (
-    <div>
-      Welcome to frontend
-      <LiveVideo color='red' />
-    </div>
-  );
+  return <div>
+    <Routes>
+      <Route path='/login' element={<Login/>} exact/>
+      <Route path='/profile' element={<Profile/>} exact/>
+      <Route path='/' element={<Home/>} exact/>
+    </Routes>
+  </div>
 }
 
 export default App;
