ec65d12c1ed78b7f18f651e1a4e157e4ed781830
NakkaGS <gtnakata@gmail.com>
2023-02-16 21:32:36 +0100

    Frontend: Login part 2

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,12 +1,29 @@
 import './style.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form } from 'formik'
 
 import { Link } from 'react-router-dom'
 
+import LoginInput from '../../components/input/loginInput'
+
+const loginInfos = {
+  email: "",
+  password: ""
+}
+
 function Login() {
+
+  const[login, setLogin] = useState(loginInfos);
+  const{ email, password } = login
+
+  console.log(login);
+
+  const handleLoginChange = (e) => {
+    const {name, value} = e.target
+    setLogin({...login,[name]:value})
+  }
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -19,16 +36,22 @@ function Login() {
           </div>
           <div className="login_2">
             <div className="login_2_wrap">
-              <Formik>
-                {
+              <Formik
+                enableReinitialize
+                initialValues={{
+                  email,
+                  password,
+                }}
+              >
+              {
 
-                    <Form>
-                      <input type="text" />
-                      <input type="text" />
-                      <button type='submit' className="blue_btn">Log In</button>
-                    </Form>
+                <Form>
+                  <LoginInput type='text' name='email' placeholder="Email address or Phone number" onChange={handleLoginChange} />
+                  <LoginInput type='password' name='password' placeholder="Password" onChange={handleLoginChange}/>
+                  <button type='submit' className="blue_btn">Log In</button>
+                </Form>
 
-                }
+              }
               </Formik>
               <Link to='/forgot' className='forgot_password'>Forgotten password?</Link>
               <div className="sign_slitter"></div>
