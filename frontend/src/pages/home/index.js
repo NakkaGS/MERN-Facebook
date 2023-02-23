94b4168e2cc25eeba61b98251fc1617ce43ab4b5
NakkaGS <gtnakata@gmail.com>
2023-02-23 11:36:19 +0100

    Frontend: Helper to hide component

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 import Header from '../../components/header/'
+import useClickOutside from '../../helpers/clickOutside'
 
 function Home() {
+  const [visible, setVisible] = useState(true)
+  const el = useRef(null)
+  useClickOutside(el, () => {
+    setVisible(false)
+    //el.current.style.display = 'none' //without the useState
+  })
+
   return (
     <div>
       <Header/>
+      {visible && <div className="card" ref={el}></div>}
     </div>
   )
 }
