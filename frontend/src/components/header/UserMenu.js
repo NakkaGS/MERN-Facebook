ba013628fac19340eaf3086c8202146f4ae0d055
NakkaGS <gtnakata@gmail.com>
2023-02-26 22:51:31 +0100

    Frontend: UserMenu part 1

--- frontend/src/components/header/UserMenu.js
+++ frontend/src/components/header/UserMenu.js
@@ -1,45 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
+import SettingsPrivacy from './userMenu/SettingsPrivacy'
 
 export default function UserMenu({ user }) {
-  return (
-    <div className="mmenu">
-        <Link to="/profile" className="mmenu_header hover3">
-            <img src={user?.picture} alt="" />
-            <div className="mmenu_col">
-                <span>
-                    {user?.first_name}
-                    {user?.last_name}
-                </span>
-                <span>See your profile</span>
-            </div>
-        </Link>
 
-        <div className="mmenu_splitter"></div>
+    const [visible, setVisible] = useState(1)
 
-        <div className="mmenu_main hover3">
-            <div className="small_circle">
-                <i className="report_filled_icon"></i>
-            </div>
+    return (
+        <div className="mmenu">
+            {visible === 0 && (
+                <div>                               
+                    <Link to="/profile" className="mmenu_header hover3">
+                        <img src={user?.picture} alt="" />
+                        <div className="mmenu_col">
+                            <span>
+                                {user?.first_name}
+                                {user?.last_name}
+                            </span>
+                            <span>See your profile</span>
+                        </div>
+                    </Link>
 
-            <div className="mmenu_col">
-                <div className="meenu_span1">Give feedback</div>
-                <div className="meenu_span2">Help us improve facebook</div>
-            </div>
+                    <div className="mmenu_splitter"></div>
+
+                    <div className="mmenu_main hover3">
+                        <div className="small_circle">
+                            <i className="report_filled_icon"></i>
+                        </div>
+
+                        <div className="mmenu_col">
+                            <div className="mmenu_span1">Give feedback</div>
+                            <div className="mmenu_span2">Help us improve facebook</div>
+                        </div>
+                    </div>
+
+                    <div className="mmenu_splitter"></div>
+
+                    <div className="mmenu_item hover3">
+                        <div className="small_circle">
+                            <i className="settings_filled_icon"></i>
+                        </div>
+                        <span>Settings & privacy</span>
+                        <div className="rArrow">
+                            <i className="right_icon"></i>
+                        </div>
+                    </div>
+
+                    <div className="mmenu_item hover3">
+                        <div className="small_circle">
+                            <i className="help_filled_icon"></i>
+                        </div>
+                        <span>Help & support</span>
+                        <div className="rArrow">
+                            <i className="right_icon"></i>
+                        </div>
+                    </div>
+
+                    <div className="mmenu_item hover3">
+                        <div className="small_circle">
+                            <i className="dark_filled_icon"></i>
+                        </div>
+                        <span>Display & Accessibility</span>
+                        <div className="rArrow">
+                            <i className="right_icon"></i>
+                        </div>
+                    </div>
+
+                    <div className="mmenu_item hover3">
+                        <div className="small_circle">
+                            <i className="logout_filled_icon"></i>
+                        </div>
+                        <span>Logout</span>
+                        <div className="rArrow">
+                            <i className="right_icon"></i>
+                        </div>
+                    </div>
+                    
+                </div>
+            )}
+            {visible === 1 && <SettingsPrivacy/>}
         </div>
-
-        <div className="mmenu_splitter"></div>
-
-        <div className="mmenu_item hover3">
-            <div className="small_circle">
-                <i className="settings_filled_icon"></i>
-            </div>
-            <span>Settings & privacy</span>
-            <div className="rArrow">
-                <i className="right_icon"></i>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
---
+++ frontend/src/components/header/userMenu/SettingsPrivacy.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function SettingsPrivacy() {
+    return (
+        <div className="absolute_wrap">
+            <div className="absolute_wrap_header">
+                <div className="circle">
+                    <i className="arrow_back_icon"></i>
+                </div>
+                Settings & privacy
+            </div>
+            <div className="mmenu_item hover3">
+                <div className="small_circle">
+                    <i className="setting_filled_icon"></i>
+                </div>
+                <span>Settings</span>
+            </div>
+        </div>
+    )
+}
