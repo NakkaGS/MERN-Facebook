361260fa2d0c2fee5392b4fc2c3b95bc20fb2dc3
NakkaGS <gtnakata@gmail.com>
2023-03-20 00:11:55 +0100

    Frontend: Reset password part 1

--- frontend/src/pages/reset/index.js
+++ frontend/src/pages/reset/index.js
@@ -10,6 +10,13 @@ import { useDispatch, useSelector } from 'react-redux'
 //Cookies
 import Cookies from 'js-cookie'
 
+//Formik
+import { Formik } from 'formik'
+
+// Components
+import LoginInput from '../../components/input/loginInput'
+import LoginForm from "../../components/login/LoginForm"
+
 export default function Reset() {
 
     const { user } = useSelector((state) => ({...state}))
@@ -18,6 +25,8 @@ export default function Reset() {
     const dispatch = useDispatch()
 
     const [visible, setVisible] = useState(0)
+    const [email, setEmail] = useState("")
+    const [error, setError] = useState("")
 
     const logout = () => {
         Cookies.set('user', "")
@@ -45,7 +54,40 @@ export default function Reset() {
                         <button className="blue_btn">Login</button>
                     </Link>
                 )}
+            </div>
 
+            <div className="reset_wrap">
+                <div className="reset_form">
+                    <div className="reset_form_header">
+                        Find your Account
+                    </div>
+                    <div className="reset_form_text">
+                        Please enter your email address or mobile number to search for your account.
+                    </div>
+                    <Formik
+                    enableReinitialize
+                    initialValues={{
+                        email,
+                    }}>
+                        <Form>
+                            <LoginForm 
+                            type="text" 
+                            name="email" 
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email address or phone number"
+                            />
+                            {error && <div className="error_text">{error}</div>}
+                            <div className="reset_form_btns">
+                                <Link to="/login" className="gray-btn">Cancel</Link>
+                                <button type="submit" className="blue_btn">Sign In</button>
+                            </div>
+                        </Form>
+                        
+
+                    
+
+                    </Formik>
+                </div>
             </div>
         </div>
     )
