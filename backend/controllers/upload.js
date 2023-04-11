f01550ec62c0fb72104f06bf63b1e869a116fcf9
NakkaGS <gtnakata@gmail.com>
2023-04-11 11:23:05 +0200

    Backend: Upload Images to Cloudinary part 1 Done

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -1,6 +1,15 @@
+const cloudinary = require("cloudinary")
+const fs = require("fs")
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+})
+
 exports.uploadImages = async (req, res) => {
   try {
-    res.json("welcome from image upload");
+    //res.json("welcome from image upload");
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- backend/middlewares/imageUpload.js
+++ backend/middlewares/imageUpload.js
@@ -1,8 +1,33 @@
+const fs = required("fs");
+
 module.exports = async function (req, res, next) {
   try {
-    console.log(req.files);
-    next();
+    if (!req.files || Object.values(req.files).flat().length === 0) {
+      return res.status(400).json({ message: "No file selected" });
+    }
+    let files = Object.values(req.files).flat();
+    files.forEach((file) => {
+      if (
+        file.mimetype !== "images/jpeg" &&
+        file.mimetype !== "images/png" &&
+        file.mimetype !== "images/gif" &&
+        file.mimetype !== "images/webp"
+      ) {
+        removeTmp(file.tempFilePath);
+        return res.status(400).json({ message: "Unsupported format " });
+      }
+      if(file.size > 1024 * 1024 * 5) {
+        return res.status(400).json({ message: "File size is too large" });
+      }
+    });
+    next()
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
+};
+
+const removeTmp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
 };
